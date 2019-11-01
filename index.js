614548c7b9c227338aa2a7ea0a438932be083a35
Apetsi Ampiah <apetsiampiah@gmail.com>
2019-11-01 09:14:02 +0000

    Complete all CRUD Actions for projects api

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-Sing along:
+const projects = require('./routes/api/projects');
 
-here's a little code I wrote, you might want to read it really slow, don't worry be happy
-in every line there may be trouble, but if you worry you make it double, don't worry, be happy
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, be happy
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just API…
-I need this code, just don't know where, perhaps should make some middleware, don't worry, be happy
+const app = express();
 
-Go code!
-*/
+// bodyparser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// cors middleware
+app.use(cors());
+
+// express middleware
+app.use(express.json());
+
+// subroutes middleware
+app.use('/api/projects', projects);
+
+app.use(express.static(__dirname + '/client/build'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/build/index.html');
+});
+
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
